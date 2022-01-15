028ef9a5f5149b8c01ff4534d18e3b8430b3dffc
vitoriajaco <vitoriajaco@gmail.com>
2022-01-15 20:51:48 -0300

    Adicionar tradução de condição

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const botao = document.querySelector('button');
 }
+traduzirCondicao = (data) => {
+    if(data.status =='unknown'){
+        return 'Não sabemos';
+    }else if(data.status =='Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    
+    }
+}
 
 gerarValorAleatorio =() => {
     return Math.floor(Math.random() * 671);
