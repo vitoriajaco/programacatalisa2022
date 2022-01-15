a66c38d83c11ca74b667b50f0a36acd9f0a17362
vitoriajaco <vitoriajaco@gmail.com>
2022-01-15 20:48:30 -0300

    Adicionar json e condição do API Rick and Morty

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ pegarPersonagem = () => {
         const nomeDoPersonagem = personagem.querySelector('#nome');
         const especie = personagem.querySelector("#especie");
         const condicao = personagem.querySelector("#status");
+
+        let numeroAleatorio = gerarValorAleatorio();
+        fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}` , {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                "Content-type": 'application/json'
+            }
+        }).then((response) => response.json()).then((data) => {
+            imagem.src = data.image;
+            imagem.alt= data.name;
+            nomeDoPersonagem.innerHTML = data.name;
+            especie.innerHTML = data.species;
+            condicao.innerHTML = traduzirCondicao(data);
+        });
+
+    }
+}
+
+botao.onclick = pegarPersonagem;
